8f6cdaa047c77eda02d03c1ba1dbc974ba836bfe
Yeiner Merino Restrepo <yeiner.merino-ext@gruposese.com>
2023-02-06 15:14:36 -0500

    Documentacion Swagger

--- MainSoft.TravelBackOffice.inventory/Program.cs
+++ MainSoft.TravelBackOffice.inventory/Program.cs
@@ -1,10 +1,9 @@
 using Leal.Core.CargaPuntos.Application.WepApi;
 using MainSoft.TravelBackOffice.Infraestructure.Core;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-var strinconecixon = builder.Configuration.GetConnectionString("TravelInventoryDB");
 // Add services to the container.
 
 builder.Services.AddDbContext<TravelInventoryContext>(options =>
@@ -12,7 +11,23 @@ builder.Services.AddDbContext<TravelInventoryContext>(options =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(swagger =>
+{
+    var contact = new OpenApiContact
+    {
+        Name = "MeisonSoft TravelOffice API"
+
+    };
+    swagger.SwaggerDoc("v1",
+    new OpenApiInfo
+    {
+        Title = "MainSoft.TravelBackOffice.InventoryApi",
+        Version = "v1",
+        Description = "MainSoft Biblioteca  API  - Documentation Services",
+        Contact = contact
+
+    });
+});
 StartUp.RegisterDI<TravelInventoryContext>(builder.Services, builder.Configuration);
 var app = builder.Build();
 

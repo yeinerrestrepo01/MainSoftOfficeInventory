9c692aa85120dcca9d587f6c99f1f1202a750f90
Yeiner Merino Restrepo <yeiner.merino-ext@gruposese.com>
2023-02-08 16:06:24 -0500

    Proyecto de test

---
+++ Mainsonft.TravelBackOffice.UnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Mainsonft.TravelBackOffice.UnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
